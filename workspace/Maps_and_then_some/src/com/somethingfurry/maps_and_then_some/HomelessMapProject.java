3e20ca16b4c7ad3c289bd147874d6a56ce74a694
Courtney Brown <redsonola@gmail.com>
2011-11-22 10:38:46 -0700

    started to add wiimote, still working on getting that library integrated

--- workspace/Maps_and_then_some/src/com/somethingfurry/maps_and_then_some/HomelessMapProject.java
+++ workspace/Maps_and_then_some/src/com/somethingfurry/maps_and_then_some/HomelessMapProject.java
@@ -10,6 +10,11 @@ import de.fhpotsdam.unfolding.utils.*;
 import de.fhpotsdam.unfolding.providers.*;
 import de.fhpotsdam.unfolding.mapdisplay.MapDisplayFactory;
 
+//import these libraries for the wii
+import lll.Loc.*;
+import lll.wrj4P5.*; 
+
+
 
 
  public class HomelessMapProject extends PApplet {
@@ -17,6 +22,7 @@ import de.fhpotsdam.unfolding.mapdisplay.MapDisplayFactory;
 	 Map map;
 	 boolean isZoomed = false; 
 	 Location phoenix = new Location(33.43f, -112.02f);
+	 Wrj4P5 wii; 
 
 	 public void setup(){
 		 size(600, 500, GLConstants.GLGRAPHICS);
@@ -27,9 +33,9 @@ import de.fhpotsdam.unfolding.mapdisplay.MapDisplayFactory;
 				  	
 		 
 		  //zoom and pan to Phoenix
-		  Double p_lat = 33.0;// + 26.0/60.0;
-		  Double p_long = 112.0;// + 4.0/60.0;
 		  map.zoomAndPanTo(phoenix, 10);
+		  
+		   wii = new Wrj4P5(this); //create wii object
 
 	 }
   
@@ -54,7 +60,50 @@ import de.fhpotsdam.unfolding.mapdisplay.MapDisplayFactory;
 		 }
 	 }
 	 
+	 public void keyPressed()
+	 {
+		 if (key == ' ')
+		 {
+			 wii.connect(1); //connect performer 1
+		 }
+	 }
 	 
+	 
+	  void buttonPressed(RimokonEvent evt, int rid) 
+	  { 
+	    
+	    if (evt.wasPressed(RimokonEvent.TWO)) 
+	    {
+	    }
+	    if (evt.wasPressed(RimokonEvent.ONE)) 
+	    {
+	    }
+	    if (evt.wasPressed(RimokonEvent.B)) 
+	    {
+	    }
+	    if (evt.wasPressed(RimokonEvent.A)) 
+	    {
+	    }
+	    if (evt.wasPressed(RimokonEvent.MINUS)) {
+	    }
+	    if (evt.wasPressed(RimokonEvent.HOME)) 
+	    {
+	
+	    } 
+	    if (evt.wasPressed(RimokonEvent.LEFT)) {
+	      }
+	    if (evt.wasPressed(RimokonEvent.RIGHT)) {
+	    }
+	    if (evt.wasPressed(RimokonEvent.DOWN)){
+
+	    }
+	    if (evt.wasPressed(RimokonEvent.UP)) 
+	    {
+	    }
+	     if (evt.wasPressed(RimokonEvent.PLUS)) 
+	     {
+	     }
+	    } 
 
 
  }
